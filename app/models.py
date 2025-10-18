9938ce3bee85f09e59b75fbb61b05c610f6dc606
jamin16604 <jamin16604@gmail.com>
2025-10-18 23:38:58 +0100

    add basmodel for summary response

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class UploadResponse(BaseModel):
     rows_processed: int
     timetaken_ms: int
 
+class SummaryResponse(BaseModel):
+    user_id: int
+    max_transaction_amount: float
+    min_transaction_amount: float
+    mean_transaction_amount: float
+    start_date: datetime
+    end_date: datetime
